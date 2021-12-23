82e4d67091d68261321f3b7669ba5e921db23f41
souhail-kaci <souhail_196@hotmail.com>
2021-12-23 16:00:30 +0100

    Add LineChart component

--- pwa/pages/propertysales/index.tsx
+++ pwa/pages/propertysales/index.tsx
@@ -1,31 +1,29 @@
-import { NextComponentType, NextPageContext } from "next";
-import { List } from "../../components/propertysale/List";
-import { PagedCollection } from "../../types/Collection";
-import { PropertySale } from "../../types/PropertySale";
-import { fetch } from "../../utils/dataAccess";
+import {NextComponentType, NextPageContext, GetStaticProps} from "next";
+import {fetch} from "../../utils/dataAccess";
 import Head from "next/head";
+import LineChart from "../../components/LineChart";
+const Page = ({data1}) => {
+    console.log(data1)
+    return (
+      <div>
+        <div>
+          <Head>
+            <title>PropertySale List</title>
+          </Head>
+        </div>
+        <LineChart/>
+      </div>
+    )
+  }
+;
 
-interface Props {
-  collection: PagedCollection<PropertySale>;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const collection = await fetch("/property_sales/average");
+  return {
+    props: {
+      data1: collection.data,
+    }
+  }
 }
-
-const Page: NextComponentType<NextPageContext, Props, Props> = ({
-  collection,
-}) => (
-  <div>
-    <div>
-      <Head>
-        <title>PropertySale List</title>
-      </Head>
-    </div>
-    <List property_sales={collection["hydra:member"]} />
-  </div>
-);
-
-Page.getInitialProps = async () => {
-  const collection = await fetch("/property_sales");
-
-  return { collection };
-};
 
 export default Page;
