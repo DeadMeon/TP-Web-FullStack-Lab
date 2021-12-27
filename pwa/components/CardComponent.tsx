b82dec8a8b5b153cca9a6eedf093b19242ae4c30
DeadMeon <devs.deadmeon@gmail.com>
2021-12-27 14:58:05 +0100

    Ajout d'une Card de react-bootstrap

--- pwa/components/CardComponent.tsx
+++ pwa/components/CardComponent.tsx
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 
 export default class CardComponent extends Component {
     render() {
-        return <div></div>
+        return <Card>
+            <Card.Body>
+                <Card.Title>Titre</Card.Title>
+                <Card.Text>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius illo fugit corrupti voluptatem dolorem autem, sequi cumque nemo minima voluptatum blanditiis nisi, ad consectetur commodi earum sapiente. In, doloremque possimus.
+                </Card.Text>
+            </Card.Body>
+        </Card>
     }
 }
