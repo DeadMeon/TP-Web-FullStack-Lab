d6d13b7ac0dcb868a859cc432dd548d0aaaf89c8
DeadMeon <devs.deadmeon@gmail.com>
2021-12-27 14:37:59 +0100

    Utilisation du Container de react-bootstrap

--- pwa/components/HeroComponent.tsx
+++ pwa/components/HeroComponent.tsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 
 
 export default class HeroComponent extends Component {
     render() {
         return <div className="jumbotron ">
-        <div className="container my-5 pt-5">
+        <Container className="my-5 pt-5">
             <h1 className="display-4">Titre</h1>
             <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, eligendi. Velit voluptate hic suscipit recusandae voluptatum atque dolores facere quam non rem fugit neque unde iusto, repellendus quas corporis porro.</p>
-        </div>
+        </Container>
     </div>
     }
 }
