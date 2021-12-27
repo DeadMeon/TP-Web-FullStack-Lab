8f04bfcb8b695ce362bdf06179bba092db909302
DeadMeon <devs.deadmeon@gmail.com>
2021-12-27 14:36:33 +0100

    Mise en forme

--- pwa/components/HeroComponent.tsx
+++ pwa/components/HeroComponent.tsx
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 
 export default class HeroComponent extends Component {
     render() {
-        return <div></div>
+        return <div className="jumbotron ">
+        <div className="container my-5 pt-5">
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, eligendi. Velit voluptate hic suscipit recusandae voluptatum atque dolores facere quam non rem fugit neque unde iusto, repellendus quas corporis porro.</p>
+        </div>
+    </div>
     }
 }
