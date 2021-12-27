4ebb281a3751ad30f468918a3822bd4d2cb189cc
DeadMeon <devs.deadmeon@gmail.com>
2021-12-27 15:00:17 +0100

    Ajout d'un container autour des cards

--- pwa/components/CardComponent.tsx
+++ pwa/components/CardComponent.tsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 
 export default class CardComponent extends Component {
     render() {
-        return <Card>
-            <Card.Body>
-                <Card.Title>Titre</Card.Title>
-                <Card.Text>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius illo fugit corrupti voluptatem dolorem autem, sequi cumque nemo minima voluptatum blanditiis nisi, ad consectetur commodi earum sapiente. In, doloremque possimus.
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        return <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Titre</Card.Title>
+                    <Card.Text>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius illo fugit corrupti voluptatem dolorem autem, sequi cumque nemo minima voluptatum blanditiis nisi, ad consectetur commodi earum sapiente. In, doloremque possimus.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
     }
 }
