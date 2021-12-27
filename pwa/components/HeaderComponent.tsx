81f28fdfd9f2681508121447cfe8181502fb5547
DeadMeon <devs.deadmeon@gmail.com>
2021-12-27 13:34:21 +0100

    Utilisation de react-bootstrap pour le header

--- pwa/components/HeaderComponent.tsx
+++ pwa/components/HeaderComponent.tsx
@@ -1,45 +1,25 @@
-import React, { Component, Props } from "react";
+import React, { Component } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import LogoComponent from "./LogoComponent";
 
 export default class HeaderComponent extends Component {
     render() {
-        return <div className="header">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-xl">
-                    <a className="navbar-brand" href="#">Container XL</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="navbarsExample07XL">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                                <div className="dropdown-menu" aria-labelledby="dropdown07XL">
-                                    <a className="dropdown-item" href="#">Action</a>
-                                    <a className="dropdown-item" href="#">Another action</a>
-                                    <a className="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                        </ul>
-                        <form className="form-inline my-2 my-md-0">
-                            <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-                        </form>
-                    </div>
-                </div>
-            </nav>
-
-            <div className="container-xl mt-n2 mb-3">
-                Matching .container-xl...
-            </div>
+        return <div className="header shadow">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+                <Container>
+                    <Navbar.Brand href="#Hero">
+                        <LogoComponent />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#PrixMetreCarre">Prix Moyen du m²</Nav.Link>
+                            <Nav.Link href="#NbVentes">Nombre de vente</Nav.Link>
+                            <Nav.Link href="#VentesParRegion">Ventes par Région</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     }
 }
