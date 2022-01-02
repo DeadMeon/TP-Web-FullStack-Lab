26916f4773ce3549a8db605821daaa0918532a84
Oxal <richard.planchon.pro@gmail.com>
2022-01-02 08:58:45 +0100

    add php unit test

--- api/tests/Api/PropertySaleTest.php
+++ api/tests/Api/PropertySaleTest.php
@@ -12,5 +12,69 @@ class PropertySaleTest extends ApiTestCase
         $response = static::createClient()->request('GET', '/property_sales/average');
 
         $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $this->assertMatchesJsonSchema([
+            "type" => "object",
+            "properties" => [
+                "data" => [
+                    "type" => "array",
+                    "items" => [
+                        "type" => "object",
+                        "properties" => [
+                            "key" => ["type"=>"string"],
+                            "value"=> ["type"=>"number"]
+                        ]
+         
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    public function testGetCount()
+    {
+        $response = static::createClient()->request('GET', '/property_sales/count/month/1-1-2017/31-12-2017');
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $this->assertMatchesJsonSchema([
+            "type" => "object",
+            "properties" => [
+                "data" => [
+                    "type" => "array",
+                    "items" => [
+                        "type" => "object",
+                        "properties" => [
+                            "key" => ["type"=>"string"],
+                            "value"=> ["type"=>"number"]
+                        ]
+         
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    public function testGetSell()
+    {
+        $response = static::createClient()->request('GET', '/property_sales/sell/2018');
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $this->assertMatchesJsonSchema([
+            "type" => "object",
+            "properties" => [
+                "data" => [
+                    "type" => "array",
+                    "items" => [
+                        "type" => "object",
+                        "properties" => [
+                            "key" => ["type"=>"string"],
+                            "value"=> ["type"=>"number"]
+                        ]
+         
+                    ]
+                ]
+            ]
+        ]);
     }
 }
